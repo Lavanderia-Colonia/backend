9789b64a7777b00020d273511c00b9b6393044c4
DiogoSAbr <diogosantanadealmeida@gmail.com>
2025-11-06 21:24:12 -0300

    feat: create OrderRepository

---
+++ api/src/main/java/com/lavanderia_colonia/api/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.lavanderia_colonia.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lavanderia_colonia.api.model.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
