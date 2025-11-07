a3812446da1e4e532c02fb1179f2e3ef9dd57882
DiogoSAbr <diogosantanadealmeida@gmail.com>
2025-11-06 21:23:56 -0300

    feat: create UserRepository

---
+++ api/src/main/java/com/lavanderia_colonia/api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.lavanderia_colonia.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lavanderia_colonia.api.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
