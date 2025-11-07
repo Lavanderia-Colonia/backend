de7f592eb7118b38d15ea4d17000aaed67cd025b
DiogoSAbr <diogosantanadealmeida@gmail.com>
2025-11-06 21:23:39 -0300

    feat: create Client entity

---
+++ api/src/main/java/com/lavanderia_colonia/api/model/Client.java
@@ -0,0 +1,82 @@
+package com.lavanderia_colonia.api.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "client")
+@Data
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 1)
+    private boolean active;
+
+    @Column(nullable = false, length = 300)
+    private String name;
+
+    @Column(length = 12)
+    private String telephone;
+
+    @Column(length = 50)
+    private String street;
+
+    @Column(length = 5)
+    private String number;
+
+    @Column(length = 80)
+    private String district;
+
+    @Column(length = 8)
+    private String zipCode;
+
+    @Column(length = 80)
+    private String complement;
+
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDate updatedAt;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+    private List<Order> orders = new ArrayList<>();
+
+    public Client() {
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDate.now();
+        updatedAt = LocalDate.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDate.now();
+    }
+}
