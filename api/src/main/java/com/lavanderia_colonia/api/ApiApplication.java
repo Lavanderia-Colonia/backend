2831a486e2e164ed4d9cd953eccfb42fa477bde5
DiogoSAbr <diogosantanadealmeida@gmail.com>
2025-11-06 21:22:08 -0300

    fix: adjusting ApiApplication

--- api/src/main/java/com/lavanderia_colonia/api/ApiApplication.java
+++ api/src/main/java/com/lavanderia_colonia/api/ApiApplication.java
@@ -2,8 +2,12 @@ package com.lavanderia_colonia.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.lavanderia_colonia.api.model")
+@EnableJpaRepositories("com.lavanderia_colonia.api.repository")
 public class ApiApplication {
 
 	public static void main(String[] args) {
