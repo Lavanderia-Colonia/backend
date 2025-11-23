2f228845c018497b75200c9ff1442f0ef87ee1d6
João Paulo Akira Sigue <joaopsigue7@gmail.com>
2025-11-23 19:14:01 -0300

    fix: client repository

--- api/src/main/java/com/lavanderia_colonia/api/repository/ClientRepository.java
+++ api/src/main/java/com/lavanderia_colonia/api/repository/ClientRepository.java
@@ -1,5 +1,9 @@
 package com.lavanderia_colonia.api.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +11,7 @@ import com.lavanderia_colonia.api.model.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    public Page<Client> findByName(String name, Pageable pageable);
 
+    public List<Client> findByActiveAndName(boolean active, String name);
 }
