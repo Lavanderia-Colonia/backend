b96e09561fcedfd7b76ad7990d3f7783fe059c62
João Paulo Akira Sigue <joaopsigue7@gmail.com>
2025-11-24 18:49:40 -0300

    fix: ProductRepository

--- api/src/main/java/com/lavanderia_colonia/api/repository/ProductRepository.java
+++ api/src/main/java/com/lavanderia_colonia/api/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.lavanderia_colonia.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.lavanderia_colonia.api.model.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    public List<Product> findAllByCode(String code);
 }
