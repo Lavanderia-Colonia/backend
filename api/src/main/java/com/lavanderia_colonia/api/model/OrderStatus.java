8b1e89ebbe6c3d7802b2963cebe31056c1ee15ad
João Paulo Akira Sigue <joaopsigue7@gmail.com>
2025-11-24 18:49:16 -0300

    feat: OrderStatus entity

---
+++ api/src/main/java/com/lavanderia_colonia/api/model/OrderStatus.java
@@ -0,0 +1,29 @@
+package com.lavanderia_colonia.api.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class OrderStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50)
+    private String name;
+
+    @OneToMany(mappedBy = "status")
+    private List<Order> orders;
+
+}
