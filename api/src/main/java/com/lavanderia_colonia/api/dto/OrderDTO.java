7dd14d0249b28bc1316c2656d03ca2af209e545f
João Paulo Akira Sigue <joaopsigue7@gmail.com>
2025-11-24 18:48:23 -0300

    feat: OrderDTO

---
+++ api/src/main/java/com/lavanderia_colonia/api/dto/OrderDTO.java
@@ -0,0 +1,18 @@
+package com.lavanderia_colonia.api.dto;
+
+import java.util.List;
+
+import com.lavanderia_colonia.api.enums.OrderType;
+
+import lombok.Data;
+
+@Data
+public class OrderDTO {
+
+    private Long id;
+    private Long clientId;
+    private OrderType finishType;
+    private String finishDeadline;
+    private List<OrderItemDTO> items;
+
+}
