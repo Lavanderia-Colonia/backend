75a3dc1fdce7057423a01cd0f8246058be7f1534
João Paulo Akira Sigue <joaopsigue7@gmail.com>
2025-11-24 18:48:15 -0300

    feat: ProductController

---
+++ api/src/main/java/com/lavanderia_colonia/api/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.lavanderia_colonia.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lavanderia_colonia.api.model.Product;
+import com.lavanderia_colonia.api.service.ProductService;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    public ResponseEntity<List<Product>> getProducts(String code) {
+        return ResponseEntity.ok(productService.findAll(code));
+    }
+}
