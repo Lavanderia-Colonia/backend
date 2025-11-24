31eceb15e2c08a49bc96da658b8ae165186ac7bf
João Paulo Akira Sigue <joaopsigue7@gmail.com>
2025-11-24 18:48:58 -0300

    feat: OrderItemColor

---
+++ api/src/main/java/com/lavanderia_colonia/api/model/OrderItemColor.java
@@ -0,0 +1,26 @@
+package com.lavanderia_colonia.api.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Entity
+@Data
+public class OrderItemColor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 40)
+    private String color;
+
+    @OneToMany(mappedBy = "color")
+    private List<OrderItem> orderItems;
+}
