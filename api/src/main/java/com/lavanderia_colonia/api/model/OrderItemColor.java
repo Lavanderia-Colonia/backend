fd296e78c3f06f384763cebfc7ecfb0a9fb4a30d
João Paulo Akira Sigue <joaopsigue7@gmail.com>
2025-11-29 19:15:54 -0300

    fix: OrderItemColor entity

--- api/src/main/java/com/lavanderia_colonia/api/model/OrderItemColor.java
+++ api/src/main/java/com/lavanderia_colonia/api/model/OrderItemColor.java
@@ -2,6 +2,8 @@ package com.lavanderia_colonia.api.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,5 +24,6 @@ public class OrderItemColor {
     private String color;
 
     @OneToMany(mappedBy = "color")
+    @JsonIgnore
     private List<OrderItem> orderItems;
 }
