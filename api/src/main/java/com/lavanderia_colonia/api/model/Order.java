7b6ce718a8ccf46dadbdffe59cdd69745f77c5bf
DiogoSAbr <diogosantanadealmeida@gmail.com>
2025-11-29 19:38:16 -0300

    fix: Order entity

--- api/src/main/java/com/lavanderia_colonia/api/model/Order.java
+++ api/src/main/java/com/lavanderia_colonia/api/model/Order.java
@@ -4,12 +4,14 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.lavanderia_colonia.api.enums.OrderType;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -34,10 +36,11 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "status_id", nullable = false)
     private OrderStatus status;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "finish_type", length = 50)
     private OrderType finishType;
 
@@ -50,12 +53,11 @@ public class Order {
     @Column(name = "updated_at")
     private LocalDate updatedAt;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "client_id", nullable = false)
     private Client client;
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
-    @JsonIgnore
     private List<OrderItem> orderItems = new ArrayList<>();
 
     public Client getClient() {
