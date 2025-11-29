f9dcfaa3e142fb1025ead9bbfb8363772f782027
João Paulo Akira Sigue <joaopsigue7@gmail.com>
2025-11-29 19:15:24 -0300

    fix: ProductController

--- api/src/main/java/com/lavanderia_colonia/api/controller/ProductController.java
+++ api/src/main/java/com/lavanderia_colonia/api/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping
     public ResponseEntity<List<Product>> getProducts(String code) {
         return ResponseEntity.ok(productService.findAll(code));
     }
