94c68305ca9699d14e8fcd654fb4d83534bb9495
João Paulo Akira Sigue <joaopsigue7@gmail.com>
2025-11-30 01:33:38 -0300

    fix: model of Client entity

--- api/src/main/java/com/lavanderia_colonia/api/model/Client.java
+++ api/src/main/java/com/lavanderia_colonia/api/model/Client.java
@@ -6,8 +6,10 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -56,7 +58,7 @@ public class Client {
     @Column(name = "updated_at")
     private LocalDate updatedAt;
 
-    @OneToMany(mappedBy = "client")
+    @OneToMany(mappedBy = "client", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnore
     private List<Order> orders = new ArrayList<>();
 
