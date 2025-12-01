aa4a66dde53034768aa8e5074f87e7cdde28d226
João Paulo Akira Sigue <joaopsigue7@gmail.com>
2025-11-30 23:38:43 -0300

    fix: Order repository

--- api/src/main/java/com/lavanderia_colonia/api/repository/OrderRepository.java
+++ api/src/main/java/com/lavanderia_colonia/api/repository/OrderRepository.java
@@ -1,16 +1,15 @@
 package com.lavanderia_colonia.api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.lavanderia_colonia.api.model.Order;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    Page<Order> findByCodeContainingIgnoreCase(String code, Pageable pageable);
+    Page<Order> findByIdContainingIgnoreCase(String code, Pageable pageable);
 
 }
