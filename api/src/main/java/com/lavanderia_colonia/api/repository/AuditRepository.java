9c82fac3ef0d5a2c0cda5ecd30b51a9a5b4f1408
João Paulo Akira Sigue <joaopsigue7@gmail.com>
2025-11-30 23:38:37 -0300

    fix: Audit repository

--- api/src/main/java/com/lavanderia_colonia/api/repository/AuditRepository.java
+++ api/src/main/java/com/lavanderia_colonia/api/repository/AuditRepository.java
@@ -1,5 +1,7 @@
 package com.lavanderia_colonia.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.lavanderia_colonia.api.model.Audit;
 
 @Repository
 public interface AuditRepository extends JpaRepository<Audit, Long> {
-
+    List<Audit> findTop10ByOrderByChangeDateDesc();
 }
