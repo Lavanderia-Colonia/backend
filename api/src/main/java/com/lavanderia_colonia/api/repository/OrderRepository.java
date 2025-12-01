2fa41ae1af48a5712f7b23cdc6320bb7f95454a2
DiogoSAbr <diogosantanadealmeida@gmail.com>
2025-11-30 23:12:45 -0300

    fix: import pageable

--- api/src/main/java/com/lavanderia_colonia/api/repository/OrderRepository.java
+++ api/src/main/java/com/lavanderia_colonia/api/repository/OrderRepository.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Repository;
 
 import com.lavanderia_colonia.api.model.Order;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
